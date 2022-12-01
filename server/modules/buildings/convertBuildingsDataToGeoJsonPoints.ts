8fb9f6ec3ee2d1bc5988bf0220a317da35c049f5
kuqmua <serezhashmakov@outlook.com>
2022-12-01 09:56:05 +0300

    convertBuildingsDataToGeoJsonPoints

---
+++ server/modules/buildings/convertBuildingsDataToGeoJsonPoints.ts
@@ -0,0 +1,29 @@
+import { BuildingsData, BuildingFeature } from './interfaces'
+
+let convertBuildingsDataToGeoJsonPoints = (
+  buildingsData: BuildingsData,
+): BuildingFeature[] => {
+  try {
+    let buildingsFeatures: BuildingFeature[] = [];
+    buildingsData.GeoCap.forEach(building => {
+      let buildingPropeties = Object.assign({}, building);
+      buildingsFeatures.push({
+        type: 'Feature',
+        geometry:
+        {
+          type: "Point",
+          coordinates: [building.latCoordinate, building.longCoordinate],
+        },
+        properties: buildingPropeties,
+        ot: "Point"//todo - use from config
+      });
+    });
+    return buildingsFeatures;
+  }
+  catch (e) {
+    e.message = 'convertBuildingsDataToGeoJsonPoints error: ' + e.message
+    throw e
+  }
+};
+
+export default convertBuildingsDataToGeoJsonPoints;
